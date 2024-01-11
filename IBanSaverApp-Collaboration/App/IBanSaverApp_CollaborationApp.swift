76fc2c025272578b8ef6519072c59fead138771c
razmaa <145837511+razmaa@users.noreply.github.com>
2024-01-11 14:06:41 +0400

    initial commit

---
+++ IBanSaverApp-Collaboration/App/IBanSaverApp_CollaborationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IBanSaverApp_CollaborationApp.swift
+//  IBanSaverApp-Collaboration
+//
+//  Created by nika razmadze on 11.01.24.
+//
+
+import SwiftUI
+
+@main
+struct IBanSaverApp_CollaborationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ IBanSaverApp-Collaboration/Screens/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  IBanSaverApp-Collaboration
+//
+//  Created by nika razmadze on 11.01.24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
