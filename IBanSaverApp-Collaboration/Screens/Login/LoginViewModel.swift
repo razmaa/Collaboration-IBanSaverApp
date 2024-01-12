5a0d5dc9ed9ec98945498446bc8d774a72b93bad
ekakele <ekaakelenjeridze@gmail.com>
2024-01-13 00:54:42 +0400

    handle login states & adjust button color accordingly

--- IBanSaverApp-Collaboration/Screens/Login/LoginViewModel.swift
+++ IBanSaverApp-Collaboration/Screens/Login/LoginViewModel.swift
@@ -7,27 +7,70 @@
 
 import Foundation
 
+// MARK: - Enum
+enum LoginState: String {
+    case emptyFields = "Email and/or password fields cannot be empty."
+    case invalidCredentials = "Invalid login credentials. Please verify your entered data and try again."
+    case validCredentials = "Login Succeeded."
+}
+
 final class LoginViewModel: ObservableObject {
     // MARK: - Properties
     @Published var email: String = ""
     @Published var password: String = ""
     @Published var isValid: Bool = false
     
+    private var UID: String {
+           do {
+               let authenticatedUser = try AuthenticationManager.shared.getAuthenticatedUser()
+               return authenticatedUser.UID
+           } catch {
+               print("Error getting authenticated user: \(error)")
+               return ""
+           }
+       }
+    
     // MARK: - Methods
     func login() {
         guard !email.isEmpty, !password.isEmpty else {
-            print("no such user found")
+            print(LoginState.emptyFields.rawValue)
             return
         }
         
         Task {
             do {
-                let returnedUserData = try await AuthenticationManager.shared.signInUser(email: email, password: password)
-                print("success")
-                print(returnedUserData)
-            } catch {
+                let authenticatedUser = try AuthenticationManager.shared.getAuthenticatedUser()
+                
+                if email.lowercased() == authenticatedUser.email?.lowercased() ?? "" && UID == authenticatedUser.UID {
+                    isValid = true
+                    print(LoginState.validCredentials.rawValue)
+                } else {
+                    print(LoginState.invalidCredentials.rawValue)
+                }
+            }  catch {
                 print("Error: \(error)")
             }
         }
     }
 }
+
+
+
+
+
+
+//    guard !email.isEmpty, !password.isEmpty else {
+//        print("no such user found")
+//        return
+//    }
+//
+//    Task {
+//        do {
+//            let returnedUserData = try await AuthenticationManager.shared.signInUser(email: email, password: password)
+//            print("success")
+//            print(returnedUserData)
+//        } catch {
+//            print("Error: \(error)")
+//        }
+//    }
+//}
