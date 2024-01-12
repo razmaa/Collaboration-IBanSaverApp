71ea35bcf89c7230d8e4269681a10813901def64
ekakele <ekaakelenjeridze@gmail.com>
2024-01-12 19:56:08 +0400

    setup LoginView UI

--- IBanSaverApp-Collaboration/Screens/Login/LoginView.swift
+++ IBanSaverApp-Collaboration/Screens/Login/LoginView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct LoginView: View {
+    // MARK: - Properties
+    @StateObject var viewModel = LoginViewModel()
+    
+    // MARK: - Body
     var body: some View {
-        Text("Test")
+        VStack(spacing: 40) {
+            TitleView(title: "Login")
+            Spacer()
+            UserAvatarView()
+            TextFieldStackView
+            Spacer()
+            setupButtonView
+            Spacer()
+        }
+        .padding()
+    }
+    
+    // MARK: - Components
+    private var setupButtonView: some View {
+        ButtonView(title: "Login", action: {}, buttonColor: viewModel.isValid ? AppColor.blue : AppColor.darkGray, viewModel: viewModel)
+    }
+    
+    private var TextFieldStackView: some View {
+        VStack(spacing: 20) {
+            TextFieldView(placeholder: .email, text: $viewModel.email)
+            TextFieldView(placeholder: .password, text: $viewModel.password, isSecure: true)
+        }
+        .padding()
     }
 }
+
 
 #Preview {
     LoginView()
