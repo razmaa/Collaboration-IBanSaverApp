75609b59c80a7a5cbafad5e81ea4d1e910dbfd21
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-01-12 14:49:20 +0400

    setup RegistrationView and RegistrationViewModel

--- IBanSaverApp-Collaboration/Screens/Registration/RegistrationScreen/RegistrationView.swift
+++ IBanSaverApp-Collaboration/Screens/Registration/RegistrationScreen/RegistrationView.swift
@@ -8,11 +8,52 @@
 import SwiftUI
 
 struct RegistrationView: View {
+    // MARK: - Properties
+    @StateObject var viewModel = RegistationViewModel()
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 40) {
+            TitleView(title: "Sign Up")
+            UserAvatarView()
+            TextFieldStackView
+            setupPasswordRestrictionsView
+            setupButtonView
+        }
+        .padding()
+    }
+    
+    // MARK: - Components
+    private var setupButtonView: some View {
+        ButtonView(title: "Register", action: {}, buttonColor: viewModel.isValid ? AppColor.blue : AppColor.darkGray, viewModel: viewModel)
+    }
+    
+    private var setupPasswordRestrictionsView: some View {
+        PasswordRestrictionsView(
+            viewModel: viewModel,
+            password: viewModel.password,
+            isValid: viewModel.isValid,
+            meetsLengthRequirement: viewModel.meetsLengthRequirement,
+            meetsUppercaseRequirement: viewModel.meetsUppercaseRequirement,
+            meetsNumberRequirement: viewModel.meetsNumberRequirement,
+            meetsSpecialCharRequirement: viewModel.meetsSpecialCharRequirement
+        )
+    }
+    
+    private var TextFieldStackView: some View {
+        VStack(spacing: 20) {
+            TextFieldView(placeholder: .email, text: $viewModel.email)
+            TextFieldView(placeholder: .password, text: $viewModel.password, isSecure: true)
+                .onChange(of: viewModel.password, { oldValue, newValue in
+                    viewModel.checkPassword(newValue)
+                })
+        }
+        .padding()
     }
 }
 
-#Preview {
-    RegistrationView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
 }
--- IBanSaverApp-Collaboration/Screens/Registration/RegistrationScreen/RegistrationViewModel.swift
+++ IBanSaverApp-Collaboration/Screens/Registration/RegistrationScreen/RegistrationViewModel.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+
+class RegistationViewModel: ObservableObject {
+    // MARK: - Properties
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var isValid: Bool = false
+    
+    //MARK: - password Properties
+    var meetsLengthRequirement: Bool {
+        return password.count >= 8
+    }
+    
+    var meetsUppercaseRequirement: Bool {
+        return password.contains(where: { $0.isUppercase })
+    }
+    
+    var meetsNumberRequirement: Bool {
+        return password.contains(where: { $0.isNumber })
+    }
+    
+    var meetsSpecialCharRequirement: Bool {
+        return password.contains(where: { "!@#$%^&*()".contains($0) })
+    }
+    
+    // MARK: - Password Methods
+    func checkPassword(_ password: String) {
+        isValid = meetsLengthRequirement && meetsUppercaseRequirement && meetsNumberRequirement && meetsSpecialCharRequirement
+    }
+}
