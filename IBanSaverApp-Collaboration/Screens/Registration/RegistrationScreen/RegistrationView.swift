2e85ec1d1346b34689a73e2c13f6765f73203cbd
likanozadze <145837358+likanozadze@users.noreply.github.com>
2024-01-12 14:43:30 +0400

    initial commit

---
+++ IBanSaverApp-Collaboration/Screens/Registration/RegistrationScreen/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  IBanSaverApp-Collaboration
+//
+//  Created by Lika Nozadze on 1/12/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
---
+++ IBanSaverApp-Collaboration/Screens/Registration/RegistrationScreen/RegistrationViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RegistrationViewModel.swift
+//  IBanSaverApp-Collaboration
+//
+//  Created by Lika Nozadze on 1/12/24.
+//
+
+import Foundation
